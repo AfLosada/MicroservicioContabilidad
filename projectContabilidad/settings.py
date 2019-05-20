d27b6ae63e97a4862d12c9b69b4520129670c02c
Aflosada <af.losada@uniandes.edu.co>
2019-05-20 16:50:41 -0500

    Arreglando el nombre

--- projectContabilidad/settings.py
+++ projectContabilidad/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contabilidad',
+    'Contabilidad',
     'rest_framework'
 ]
 
