bed45c8499bd26de0971ab0db096b2e93ce95da0
Aflosada <af.losada@uniandes.edu.co>
2019-05-20 23:49:39 -0500

    hola

--- Contabilidad/views.py
+++ Contabilidad/views.py
@@ -39,8 +39,8 @@ def generarReporte(request):
     facturas = db['facturas']
     fechaPrueba = datetime.datetime(2019, 11, 1)
     print(fechaPrueba)
-    print(request.POST.get("fecha", fechaPrueba))
-    delDia = facturas.find({"fecha": request.POST.get("fecha", fechaPrueba)})
+    print(request.POST.get("fecha", "2012-04-23T18:0:0:0"))
+    delDia = facturas.find({"fecha": request.POST.get("fecha", "2012-04-23T18:0:0:0")})
     # delDia = facturas.find({"fecha" : fechaPrueba})
     # # delDia = facturas.find({})
     result = []
